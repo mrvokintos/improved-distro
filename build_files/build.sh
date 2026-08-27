d16e047bec689f8e1dad3c48321c2c743a21cd20
mrvokintos <184386663+mrvokintos@users.noreply.github.com>
2026-08-27 14:52:09 +0300

    Build personalized Bazzite image

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,16 +12,62 @@ cp -avf "/ctx/system_files"/. /
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux
+# Remove Waydroid itself. Bazzite's helper files are not owned by the
+# Waydroid RPM, so they are cleaned up separately below.
+dnf5 remove -y waydroid
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+# PortProtonQt is distributed through COPR. Keep the repository disabled in
+# the finished image; it is enabled again on every image build.
+dnf5 -y copr enable boria138/portproton
 
-#### Example for enabling a System Unit File
+# Fedora/RPM Fusion packages and applications from the repository files in
+# system_files/etc/yum.repos.d/.
+dnf5 install -y \
+	brave-browser \
+	code \
+	discord \
+	firefox \
+	firefox-langpacks \
+	portprotonqt \
+	throne
 
-systemctl enable podman.socket
+dnf5 -y copr disable boria138/portproton
+dnf5 config-manager setopt \
+	brave-browser.enabled=0 \
+	code.enabled=0 \
+	throne-repo.enabled=0
+
+### Remove Bazzite's Waydroid integration
+
+sed -i '\|82-bazzite-waydroid.just|d' /usr/share/ublue-os/justfile
+
+if [[ -f /usr/share/yafti/yafti.yml ]]; then
+	sed -i \
+		'/^      - id: "waydroid"$/,/^  - title:/{/^  - title:/!d;}' \
+		/usr/share/yafti/yafti.yml
+fi
+
+rm -f \
+	/etc/default/waydroid-launcher \
+	/usr/bin/waydroid-choose-gpu \
+	/usr/bin/waydroid-launcher \
+	/usr/libexec/waydroid-container-restart \
+	/usr/libexec/waydroid-container-start \
+	/usr/libexec/waydroid-container-stop \
+	/usr/libexec/waydroid-fix-controllers \
+	/usr/share/applications/waydroid-container-restart.desktop \
+	/usr/share/polkit-1/actions/org.bazzite.waydroid.policy \
+	/usr/share/polkit-1/rules.d/30-waydroid.rules \
+	/usr/share/ublue-os/just/82-bazzite-waydroid.just \
+	/usr/share/ublue-os/motd/tips/20-bazzite.md
+
+rm -rf \
+	/usr/lib/waydroid \
+	/usr/share/ublue-os/waydroid
+
+### Prefer Fedora's Firefox RPM over Bazzite's default Firefox Flatpak
+
+if [[ -f /usr/share/ublue-os/bazzite/flatpak/install ]]; then
+	sed -i '/^org\.mozilla\.firefox$/d' \
+		/usr/share/ublue-os/bazzite/flatpak/install
+fi
